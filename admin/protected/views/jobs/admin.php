79c21eab6e29967dcf133da6950f35db143b3e8c
Pham Ngoc Sinh <smagic39@gmail.com>
2014-05-09 01:59:40 +0000

    Model for add testimonial front

--- admin/protected/views/jobs/admin.php
+++ admin/protected/views/jobs/admin.php
@@ -41,9 +41,7 @@ $('.search-form form').submit(function(){
     ));
     ?>
 </div><!-- search-form -->
-<?php
-JobResumes::Model()->getAppliccation(16);
-?>
+
 <?php
 $this->widget('zii.widgets.grid.CGridView', array(
     'id' => 'jobs-grid',
